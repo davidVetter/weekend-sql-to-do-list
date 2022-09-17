fde19c7c0a8d39b2ed52f3cfc2b25663ef5f27a1
David Vetter <ddvetter23@gmail.com>
2022-09-17 10:55:46 -0500

    added POST to add new task to DB, tested working in POSTMAN

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -6,13 +6,35 @@ const pool = require('../modules/pool'); // import pool
 //GET for all tasks sorted by tasks DATE ADDED - newly or oldest is passed as param in url
 router.get('/:sortKey', (req, res) => {
     let sort = req.params.sortKey.toUpperCase();
+    console.log('This is sort: ', sort);
     let query = `SELECT * FROM "tasks" ORDER BY "dateAdded" ${sort}`;
     pool.query(query).then(result => res.send(result.rows));
 });
 
 //POST adding a new task to DB
+router.post('/', (req, res) => {
+    let newTask = req.body; // gets req.body (data sent in through POST)
+    console.log('Adding this task: ', newTask); // logs newTask being added to DB
+    if(!newTask.taskName || !newTask.taskDescription) {
+        res.sendStatus(400); // sends 'bad request' status if either input is missing
+        return; // ends function so multiple responses aren't attempted
+    };
+    if(!newTask.dueDate) {
+        newTask.dueDate = null; // allows dueDate to be an optional add when creating new task
+    };
+    //SQL query to use on DB
+    let query = `INSERT INTO "tasks" ("taskName", "taskDescription", "dueDate")
+                 VALUES ($1, $2, $3);`;
+    // perform query on DB
+    pool.query(query, [newTask.taskName, newTask.taskDescription, newTask.dueDate])
+        .then(result => res.sendStatus(201)).catch(error => {
+            console.log('Error adding new task!', error);
+            res.sendStatus(500);
+        });
+});
 
 // PUT for updating if a task is completed ONLY
+
 
 // PUT for updating any info on a task (edit mode)
 

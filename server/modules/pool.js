6dc5ec9808791491f4889cbc47ccfe6bfefda264
David Vetter <ddvetter23@gmail.com>
2022-09-23 07:58:24 -0500

    added heroku config, pushed database to heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,25 @@
 const pg = require('pg'); // import postgres
+const url = require('url');
+
+let config = {};
 
 //configuration for for database to connect to
-const config = {
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
+if (process.env.DATABASE_URL) {
+    config = {
+      // We use the DATABASE_URL from Heroku to connect to our DB
+      connectionString: process.env.DATABASE_URL,
+      // Heroku also requires this special `ssl` config
+      ssl: { rejectUnauthorized: false },
+    };
+  } else {
+    config = {
+        database: 'weekend-to-do-app',
+        host: 'localhost',
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 30000
 };
+}
 
 const pool = new pg.Pool(config); // initialze the pool using db from config
 
